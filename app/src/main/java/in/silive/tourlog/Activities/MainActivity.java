4e5d923380b49e1b4f2d9386ae5f1653cc1569c8
akriti <akritikts@gmail.com>
2017-03-03 22:11:58 +0530

    main act layout updated

--- app/src/main/java/in/silive/tourlog/Activities/MainActivity.java
+++ app/src/main/java/in/silive/tourlog/Activities/MainActivity.java
@@ -2,14 +2,20 @@ package in.silive.tourlog.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import in.silive.tourlog.R;
 
 public class MainActivity extends AppCompatActivity {
+    EditText user_location;
+    ListView list_places;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        user_location = (EditText)findViewById(R.id.user_location);
+        list_places = (ListView)findViewById(R.id.list_places);
     }
 }
